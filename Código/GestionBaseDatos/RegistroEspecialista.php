165d9ec523fe74a5d48cba30050433b814d974d9
Len-26 <elenadalmau6@gmail.com>
2024-10-17 21:01:00 +0200

    main

--- Código/GestionBaseDatos/RegistroEspecialista.php
+++ Código/GestionBaseDatos/RegistroEspecialista.php
@@ -6,28 +6,28 @@
                 CuentaBancaria_Especialista, Cuota_Especialista)
             VALUES
                 ('35710210X','634973323','jgil@gmail.com','Jose', 'Gil', 'Tarragona','110','C/', '20/11/1997', 'ES1212341234120123456789', '90,85'),
-                ('84276108A','649561795','sstone@gmail.com','Santiago', 'Stone', 'del Carrilet','114','Av.', '12/04/1995', 'ES2476831095714658203076', '75,15'),
-    
-                ('55288172J','645057456','bstone@gmail.com','Blanca', 'Stone', 'de Cantàbria','12','C/', '12/04/1995', 'ES8188358801246553965472', '87,35'),
+                
+                ('84276108A','649561795','slopez@gmail.com','Santiago', 'Lopez', 'del Carrilet','114','Av.', '12/04/1995', 'ES2476831095714658203076', '75,15'),
 
-                ('58507369S','678952037','asuarez@gmail.com','Antonio', 'Suárez', 'del Comte Borrell','307','C/', '12/04/1995', 'ES8501517856703099748789', '6'),
+                ('55288172J','605403214','bmartinez@gmail.com','Blanca', 'Martinez', 'de Cantàbria','12','C/', '31/10/1986', 'ES8188358801246553965472', '87,35'),
 
+                ('58507369S','678952037','asuarez@gmail.com','Antonio', 'Suárez', 'del Comte Borrell','307','C/', '17/10/1993', 'ES8501517856703099748789', '99,99'),
 
-                ('61156217E','696187474','ofernandez@gmail.com','Ona', 'Fernandez', 'de Leonardo da Vinci','24','C/', '12/04/1995', 'ES0420902044318259662746', '6'),
+                ('61156217E','696187474','cfernandez@gmail.com','Cecilia', 'Fernandez', 'de Leonardo da Vinci','24','C/', '26/05/1985', 'ES0420902044318259662746', '120,05'),
 
-                ('58507369S','649561795','asuarez@gmail.com','Antonio', 'Suárez', 'de les Roses','','Pl', '12/04/1995', 'ES8501517856703099748789', '6'),
+                ('17801516E','668954318','vherrera@gmail.com','Víctor', 'Herrera', 'de les Roses','','Pl', '03/12/1991', 'ES6520565729624448284452', '97,00'),
 
-                ('58507369S','649561795','asuarez@gmail.com','Antonio', 'Suárez', 'de les Roses','','Pl', '12/04/1995', 'ES8501517856703099748789', '6'),
+                ('25899345B','668436584','ngutierrez@gmail.com','Natalia', 'Gutierrez', 'del Doctor Figarola','16','C/', '20/03/1987', 'ES3401043936410285983437', '88,88'),
 
-                ('58507369S','649561795','asuarez@gmail.com','Antonio', 'Suárez', 'de les Roses','','Pl', '12/04/1995', 'ES8501517856703099748789', '6'),
+                ('36987937G','665412001','atorres@gmail.com','Alicia', 'Torres', 'dels Pensaments','1','C/', '27/04/1995', 'ES3301313873110191038048', '92,15'),
 
-                ('58507369S','649561795','asuarez@gmail.com','Antonio', 'Suárez', 'de les Roses','','Pl', '12/04/1995', 'ES8501517856703099748789', '6'),
+                ('40441106E','651300210','sdiaz@gmail.com','Sergio', 'Diaz', 'de les Delicies','31','C/', '08/11/1983', 'ES7301087926891955150150', '110,28'),
 
     $Resultado= mysqli_multi_query($conn, $sql_ESPECIALISTAS);
 
     if ($Resultado)
     {
-        echo "5 ESPECIALISTAS añadidos correctamente";
+        echo "9 ESPECIALISTAS añadidos correctamente";
         
     } 
     else 
