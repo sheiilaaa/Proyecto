e0aa0017c06f5fee88fec3947053ffed3d6d1902
Len-26 <elenadalmau6@gmail.com>
2024-10-24 19:44:52 +0200

    Create ConfirmacionModificar.php

---
+++ Código/ConfirmacionModificar.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<!--CABECERA-->
+<header id="header">
+    <nav class="menu">
+        <div class="logo">
+            <img src="img/logo.png">
+            <a href="#" class="btn-menu" id="btn-menu"><i class="icono fa fa-bars" aria-hidden="true"></i></a> 
+        </div>
+        <div id="enlaces" class="enlaces" >
+            <a  href="Inicio.php"><i class="fa fa-home" aria-hidden="true"></i>Inicio</a>
+            <a  href="ComoTrabajamos.php"><i class="fa fa-info" aria-hidden="true"></i> Como trabajamos</a>
+            <a  href="Contacto.php"><i class="fa fa-briefcase" aria-hidden="true"></i>Puesta en contacto</a>
+            <a  href="ListadoEspecialista.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>Listado especialista</a>
+            <a  href="Calendario.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>Calendario</a>
+        </div>
+    </nav>
+</header>
+
+
+<h3>Gracias por su registro, su usuario ha sido guardado</h3>
+
+
+<!-- PIE DE PAGINA -->
+<footer>
+Todos los derechos reservados | Coaching SL Copyright © 2024
+</footer>
+    
+</body>
+</html>
