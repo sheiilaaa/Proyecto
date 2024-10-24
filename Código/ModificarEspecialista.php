ca6865fc664f3a5c6bbcda66fa26e4a540d79c9c
Len-26 <elenadalmau6@gmail.com>
2024-10-24 19:42:27 +0200

    Update ModificarEspecialista.php

--- Código/ModificarEspecialista.php
+++ Código/ModificarEspecialista.php
@@ -1,28 +1,13 @@
 <!DOCTYPE html>
 <html lang="es">
-    
-    <head>
-        
-        <meta charset="utf-8">
-        
-        <title> Listado Especialista </title>    
-        
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         
-        <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
-        <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
-        
-        <!-- Link hacia el archivo de estilos css -->
-        <link rel="stylesheet" href="css/estilo.css">
-        <!-- Link hacia el archivo de estilos de bootstrap -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ComoTrabajamos</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
 
-        <script src="script.js"></script>
-    </head>
-
-    <body class="fondo">
-
-
+<body>
 <!--CABECERA-->
 <header id="header">
     <nav class="menu">
@@ -41,82 +26,82 @@
 </header>
 
 
-
-
-
-    <!-- Link hacia las librerias jsp de bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-      <div class="titulo2"> Listado de especialistas </div>
-      <div id="contenedor2">
-
+<div class="titulo2">Modificar Especialista </div>
 <?php
 	session_start();
-  include ("./GestionBD/conexion.php");
-  $sql="SELECT * FROM articulos";  
-  $result = mysqli_query($conn,$sql);
-  
-  if (mysqli_num_rows($result)>0){ //Si encuentra resultados
+    include ("./GestionBD/conexion.php");
 
-  ?>
-    <a href="AltaArticulo.php?"> Alta Artículo</a> <!--- enlace de la izquierda --->
-    <br>
-  <?php
-    while($row = mysqli_fetch_assoc($result)){
-
-	?>
-           <div>
-                <div class="card" style="width: 18rem; margin: 10px;">
-                    <img src="<?php echo $row['Foto']?>" class="card-img-top" alt="img3">
-                    <div class="card-body">
-                      <h5 class="card-title">Nombre: <?php echo $row['Nombre']?></h5>
-                      <p class="card-text">Descripcion: <?php echo $row['Descripcion']?></p>
-                      <p class="card-text">Precio: <?php echo $row['Precio']?><label name="Precio4"></label></p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item">
-                        <label for="cantidad4">Cantidad: </label>
-                        <input  type="number" id="cantidad4" style="width: 80px;">
-                        <input  type="button" id="cantidad4" name="Añadir4" class="boton" value="Añadir" onclick="ProdAdd('prod4','154')">
-                      
-                      <?php
-                        if ($_SESSION['Usuario']== "Admin"){ //Si el usuario es Admin
-                      ?>
-                                <!--- enclaces para modificar o eliminar cada articulo --->
-                          <a href="ModificarEspecialista.php?id=<?php $row['id_Articulo']?>"> Modificar </a> 
-                          <br>
-                          <a href="EliminarEspecialista.php?id=<?php $row['id_Articulo']?>"> Eliminar </a>
-                          <br>
-                      <?php
-						            }
-                      ?>
-                      </li>
-                    </ul>
-                </div>
-            </div>
-            <br>
-<?php
-    }
-      if ($_SESSION['Usuario']== "Admin"){ //Si el usuario es Admin, permite añadir articulos
-      ?>
-      <br>
-      <a href="AltaArticulo.php?"> Alta Artículo</a>  <!--- enlace de la derechca --->
-<?php
-  }
-}
-?>      
-      </div> <!--- cierra div contenedor2--->
-          
-        <span id="contenedor2">
-            <h5><a href="login.php"> Inicio </a></h5>
-        </span>  
+    if(isset($_REQUEST['Modificar'])){
+        $id_Art=$_REQUEST['id_Articulo'];
+        $Nombre=$_REQUEST['Nombre'];
+        $Descripcion=$_REQUEST['Descripcion'];
+        $Foto=$_REQUEST['Foto'];
+        $Precio=$_REQUEST['Precio'];
+		$Actualizar = "UPDATE Articulos SET Nombre='$Nombre', Descripcion='$Descripcion', Foto='$Foto', Precio='$Precio'
+        WHERE id_Articulo='$id_Art';";
         
+        if(mysqli_query($conn,$Actualizar)){
+            header("Location:Modificar_Conf.php?Nombre=$Nombre");
+         } else{
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    
+    }
+
+	if(isset($_REQUEST['id'])){ 
+        $id_Articulo=$_REQUEST['id'];     
+        $sql= " SELECT * FROM Articulos WHERE id_Articulo= '$id_Articulo'; ";
+		$result= mysqli_query($conn,$sql);
+
+            if(mysqli_num_rows($result)>0)
+			{
+?>
+              <div id="contenedor">
+				<div id="central">
+                <div id="login">
+                    <div class="titulo">
+                        Modifica el Articulo:
+                    </div>
+                    <form id="ModificaArticulo" action="" method="post">
+                        <!--- ID readonly, ya que es el unico que no se podrá modificar --->
+                        <label for="id_Articulo">Id del Articulo:</label>
+                            <input type="text" id="id_Articulo" readonly name="id_Articulo" class="caja" autofocus value = '<?php echo $row['id_Articulo'] ?>' placeholder="id_Articulo">
+                        <label for="Nombre">Nombre:</label>
+                            <input type="text" id="Nombre" name="Nombre" class="caja" autofocus required value = '<?php echo $row['Nombre'] ?>' placeholder="Nombre">
+                        <label for="Descripcion">Descripcion:</label>
+                            <input type="text" id="Descripcion" name="Descripcion" class="caja" required value = '<?php echo $row['Descripcion'] ?>' placeholder="Descripcion">
+                        <label for="Foto">Foto:</label>
+                            <input type="image" name="Foto" id="Foto" class="caja" value = '<?php echo $row['Foto'] ?>' placeholder="Foto">
+                        <label for="Precio">Precio: </label>
+                            <input type="number" name="Precio"  id="Precio" class="caja" required value = '<?php echo $row['Precio'] ?>' placeholder="Precio">
+                        
+                        <button type="submit" title="Modificar" name="Modificar">Modificar</button>
+                    </form>
+                    <div class="pie-form">
+                        <a href="catalogo.php">Volver</a>
+                    </div>
+                </div>
+            </div>    
+        </div>
+<?php
+            }
+            else{
+                echo "Error articulo no encontrado: " . $sql . "<br>" . mysqli_error($conn);
+            }
+      }
+?>
+
+
+
+
+
+ConfirmacionModificar.php
+<button><>
 
 <!-- PIE DE PAGINA -->
 <footer>
 Todos los derechos reservados | Coaching SL Copyright © 2024
 </footer>
-
-
-    </body>
+    
+</body>
 </html>
