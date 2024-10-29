44bb11ac96a03b08e69cdd5b8ba43efb3a870d83
Len-26 <elenadalmau6@gmail.com>
2024-10-29 23:16:18 +0100

    Update BaseDeDatos.sql

--- MySQL - Base de datos/BaseDeDatos.sql
+++ MySQL - Base de datos/BaseDeDatos.sql
@@ -1,19 +1,21 @@
-/*CREATE SCHEMA COACHING;*/
+CREATE SCHEMA COACHING;
 
 USE COACHING;
 
 CREATE TABLE CLIENTES(
 	ID_Cliente INT AUTO_INCREMENT PRIMARY KEY,
 	DNI_Cliente VARCHAR(9) UNIQUE NOT NULL,
-	NumTelefono_Cliente INT(9) NOT NULL,
+	NumTelefono_Cliente INT NOT NULL,
 	Correo_Cliente VARCHAR(100) NOT NULL,
 	Nombre_Cliente VARCHAR (100) NOT NULL,
 	Apellido_Cliente VARCHAR (100) NOT NULL,
 
 	NombreVia_Cliente VARCHAR(100) NULL,
-	NumeroVia_Cliente INT (4) NULL,
-	TipoVia_Cliente ENUM("C/", "Paseo", "Av.") NULL,
-	FechaNacimiento_Cliente DATE NOT NULL
+	NumeroVia_Cliente INT NULL,
+	TipoVia_Cliente ENUM('C/', 'Paseo', 'Av.') NULL,
+	FechaNacimiento_Cliente DATE NOT NULL,
+    
+    Contrasena_Cliente VARCHAR(100)
 
 	/*Claves Foráneas*/
 );
@@ -21,27 +23,30 @@ CREATE TABLE CLIENTES(
 CREATE TABLE ESPECIALISTAS(
 	ID_Especialista INT AUTO_INCREMENT PRIMARY KEY,
 	DNI_Especialista VARCHAR(9) UNIQUE NOT NULL,
-	NumTelefono_Especialista INT(9) NOT NULL,
+	NumTelefono_Especialista INT NOT NULL,
 	Correo_Especialista VARCHAR(100) NOT NULL,
 	Nombre_Especialista VARCHAR (100) NOT NULL,
 	Apellido_Especialista VARCHAR (100) NOT NULL,
 
 	NombreVia_Especialista VARCHAR(100) NOT NULL,
-	NumeroVia_Especialista INT (4) NOT NULL,
-	TipoVia_Especialista ENUM("C/", "Paseo", "Av.") NOT NULL,
+	NumeroVia_Especialista INT NOT NULL,
+	TipoVia_Especialista ENUM('C/', 'Paseo', 'Av.') NOT NULL,
 
-	CuentaBancaria_Especialista INT(20) NOT NULL,/*ENCRIPTAR*/
-	FechaNacimiento_Especialista INT(8) NOT NULL,
+	CuentaBancaria_Especialista INT NOT NULL,/*ENCRIPTAR*/
+	FechaNacimiento_Especialista INT NOT NULL,
 
-	Cuota_Especialista DECIMAL NOT NULL
+	Cuota_Especialista DECIMAL NOT NULL,
+
+    Contrasena_Especialista VARCHAR(100)
 
 	/*Claves Foráneas*/
 );
 
-CREATE TABLE ESPECIALITSA_ESPECIALIDAD(
+CREATE TABLE ESPECIALISTA_ESPECIALIDAD(
 	/*Claves Foráneas*/
-	ID_Especialista_EspeEspe INT PRIMARY KEY,
-	ID_Especialidad_EspeEspe INT PRIMARY KEY
+	ID_Especialista_EspeEspe INT,
+	ID_Especialidad_EspeEspe INT,
+    PRIMARY KEY (ID_Especialista_EspeEspe, ID_Especialidad_EspeEspe)
 );
 
 
@@ -59,16 +64,16 @@ CREATE TABLE CITAS(
 
 CREATE TABLE ESPECIALIDAD(
 	ID_Especialidad INT AUTO_INCREMENT PRIMARY KEY,
-	Especialidad_Especialista ENUM ("Coaching Empresarial","Coaching Personal","Coaching con Inteligencia Emocional",
-									"Coaching Deportivo","Coaching Ontológico","Coaching Cognitivo",
-                                    "Coaching PNL (Programación Neurolingüística)","Coaching Coercitivo") UNIQUE
+	Especialidad_Especialista ENUM ('Coaching Empresarial','Coaching Personal','Coaching con Inteligencia Emocional',
+									'Coaching Deportivo','Coaching Ontológico','Coaching Cognitivo',
+                                    'Coaching PNL (Programación Neurolingüística)','Coaching Coercitivo') UNIQUE
 	/*Claves Foráneas*/
 );
 
 CREATE TABLE PAGOS(
 	ID_Pago INT AUTO_INCREMENT PRIMARY KEY,
 	Estado_Pago BOOLEAN NOT NULL, /*BOOLEAN --> CHECK BOX*/
-	Metodos_Pago ENUM("Efectivo","Tarjeta_Credito") NOT NULL,
+	Metodos_Pago ENUM('Efectivo','Tarjeta_Credito') NOT NULL,
 	Fecha_Pago DATETIME NOT NULL,
 	Cantidad_Pago INT NOT NULL,
 
@@ -76,14 +81,14 @@ CREATE TABLE PAGOS(
     ID_Pago_Cliente INT
 );
 
-
 CREATE TABLE DISPONIBILIDAD_ESPECIALISTA(
-	Fecha_Disponibilidad DATE PRIMARY KEY ,
-	Hora_Disponibilidad TIME PRIMARY KEY,
-	Disponibilidad_Especialista ENUM("SI","NO") NOT NULL,
+	Fecha_Disponibilidad DATE,
+	Hora_Disponibilidad TIME,
+	Disponibilidad_Especialista ENUM('SI','NO') NOT NULL,
     
 	/*Claves Foráneas*/
-	ID_Especialista_DispoEspe INT PRIMARY KEY
+	ID_Especialista_DispoEspe INT,
+    PRIMARY KEY (Fecha_Disponibilidad, Hora_Disponibilidad, ID_Especialista_DispoEspe)
 );
 
 /*RELACIONES*/
@@ -101,5 +106,5 @@ ALTER TABLE CITAS ADD
 ALTER TABLE PAGOS ADD
 	CONSTRAINT FK_PAGOS_CLIENTE FOREIGN KEY (ID_Pago_Cliente) REFERENCES CLIENTES (ID_Cliente) ON UPDATE CASCADE;
 
-ALTER TABLE DISPONIBILDAD_ESPECIALISTA ADD
+ALTER TABLE DISPONIBILIDAD_ESPECIALISTA ADD
 	CONSTRAINT FK_DISPONIBILIDAD_ESPECIALISTA_ESPECIALISTAS FOREIGN KEY (ID_Especialista_DispoEspe) REFERENCES ESPECIALISTAS (ID_Especialista) ON UPDATE CASCADE;
