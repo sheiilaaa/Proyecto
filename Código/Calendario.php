4887e5de1922603efd856299063bc1bc4fa30e0a
Len-26 <elenadalmau6@gmail.com>
2024-10-31 19:28:03 +0100

    main

--- Código/Calendario.php
+++ Código/Calendario.php
@@ -5,48 +5,77 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ComoTrabajamos</title>
     <link rel="stylesheet" href="css/estilo.css">
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+        }
+        .centrar_contenido {
+            display: flex;
+            width: 70%;
+        }
+        .calendario {
+            display: flex;
+            flex-wrap: wrap; 
+            justify-content: space-between; 
+        }
+        .dias_calendario {
+            margin: 5px;
+            border: solid 1px #ccc;
+            border-radius: 5px;
+            width: calc(14.2857% - 10px);
+            text-align: center; 
+            padding: 20px; 
+        }
+    </style>
 </head>
 
 <body>
-<!--CABECERA-->
-<header id="header">
-    <nav class="menu">
-        <div class="logo">
-            <img src="img/logo.png">
-            <a href="#" class="btn-menu" id="btn-menu"><i class="icono fa fa-bars" aria-hidden="true"></i></a> 
-        </div>
-        <div id="enlaces" class="enlaces" >
-            <a  href="Inicio.php"><i class="fa fa-home" aria-hidden="true"></i>Inicio</a>
-            <a  href="ComoTrabajamos.php"><i class="fa fa-info" aria-hidden="true"></i> Como trabajamos</a>
-            <a  href="Contacto.php"><i class="fa fa-briefcase" aria-hidden="true"></i>Puesta en contacto</a>
-            <a  href="ListadoEspecialista.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>Listado especialista</a>
-            <a  href="Calendario.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>Calendario</a>
-        </div>
-    </nav>
-</header>
 
-<main class="centrar_contenido">
-    <section class="calendario"></section>
-        <?php
-            $i;
-            $contador = 1;
-            for($i = 0; $i <35; $i++){
-                echo "<div dias_calendario>$contador</div>"; /*cuando sea multiple de siete, saltar */
-                if($contador == 31){
-                    $contador = 1;
-                }
-                else{
-                    $contador++;
-                }
-            }
-        ?>
-    </section>
-</main>
+    <!--CABECERA-->
+    <header id="header">
+        <nav class="menu">
+            <div class="logo">
+                <img src="img/logo.png">
+                <a href="#" class="btn-menu" id="btn-menu"><i class="icono fa fa-bars" aria-hidden="true"></i></a> 
+            </div>
+            <div id="enlaces" class="enlaces" >
+                <a  href="Inicio.php"><i class="fa fa-home" aria-hidden="true"></i>Inicio</a>
+                <a  href="ComoTrabajamos.php"><i class="fa fa-info" aria-hidden="true"></i> Como trabajamos</a>
+                <a  href="Contacto.php"><i class="fa fa-briefcase" aria-hidden="true"></i>Puesta en contacto</a>
+                <a  href="ListadoEspecialista.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>Listado especialista</a>
+                <a  href="Calendario.php"><i class="fa fa-envelope-o" aria-hidden="true"></i>Calendario</a>
+            </div>
+        </nav>
+    </header>
 
-<!-- PIE DE PAGINA -->
-<footer>
-Todos los derechos reservados | Coaching SL Copyright © 2024
-</footer>
+    <main class="centrar_contenido">
+        <section class="calendario">
+            <?php
+                $diasEnElMes = 31;
+                $primerDiaDeLaSemana = 0; 
+
+
+                for ($i = 0; $i < $primerDiaDeLaSemana; $i++) {
+                    echo "<div class='dias_calendario'></div>";
+                }
+
+                for ($contador = 1; $contador <= $diasEnElMes; $contador++) {
+                    echo "<div class='dias_calendario'>$contador</div>";
+                }
+
+                $totalDias = $primerDiaDeLaSemana + $diasEnElMes;
+                for ($i = $totalDias; $i < 35; $i++) {
+                    echo "<div class='dias_calendario'></div>";
+                }
+            ?>
+        </section>
+    </main>
+
+    <!-- PIE DE PAGINA -->
+    <footer>
+        Todos los derechos reservados | Coaching SL Copyright © 2024
+    </footer>
     
 </body>
 </html>
--- Código/ModificarEspecialista.php
+++ Código/ModificarEspecialista.php
@@ -26,22 +26,37 @@
 </header>
 
 
+<!-- QUE ES? -->
 <div class="titulo2">Modificar Especialista </div>
 <?php
 	session_start();
     include ("./GestionBD/conexion.php");
 
     if(isset($_REQUEST['Modificar'])){
-        $id_Art=$_REQUEST['id_Articulo'];
-        $Nombre=$_REQUEST['Nombre'];
-        $Descripcion=$_REQUEST['Descripcion'];
-        $Foto=$_REQUEST['Foto'];
-        $Precio=$_REQUEST['Precio'];
-		$Actualizar = "UPDATE Articulos SET Nombre='$Nombre', Descripcion='$Descripcion', Foto='$Foto', Precio='$Precio'
-        WHERE id_Articulo='$id_Art';";
+        $ID_Especialista=$_REQUEST['ID_Especialista'];
+        
+        $DNI_Especialista=$_REQUEST['DNI_Especialista'];
+        $Nombre_Especialista=$_REQUEST['Nombre_Especialista'];
+        $Apellido_Especialista=$_REQUEST['Apellido_Especialista'];
+        $FechaNacimiento_Especialista=$_REQUEST['FechaNacimiento_Especialista'];
+        $NumTelefono_Especialista=$_REQUEST['NumTelefono_Especialista'];
+        $Correo_Especialista=$_REQUEST['Correo_Especialista'];
+        $TipoVia_Especialista=$_REQUEST['TipoVia_Especialista'];
+        $NombreVia_Especialista=$_REQUEST['NombreVia_Especialista'];
+        $NumeroVia_Especialista=$_REQUEST['NumeroVia_Especialista'];
+        $CuentaBancaria_Especialista=$_REQUEST['CuentaBancaria_Especialista'];
+        $Cuota_Especialista=$_REQUEST['Cuota_Especialista'];
+        $Contrasena_Especialista=$_REQUEST['Contrasena_Especialista'];
+
+		$Actualizar = "UPDATE ESPECIALISTAS SET Nombre_Especialista='$Nombre_Especialista', Apellido_Especialista='$Apellido_Especialista',
+                        FechaNacimiento_Especialista='$FechaNacimiento_Especialista', NumTelefono_Especialista='$NumTelefono_Especialista'
+                        Correo_Especialista='$Correo_Especialista', TipoVia_Especialista='$TipoVia_Especialista', NombreVia_Especialista='$NombreVia_Especialista',
+                        NumeroVia_Especialista='$NumeroVia_Especialista', CuentaBancaria_Especialista='$CuentaBancaria_Especialista', Cuota_Especialista='$Cuota_Especialista',
+                        Contrasena_Especialista='$Contrasena_Especialista'
+        WHERE DNI_Especialista='$DNI_Especialista';";
         
         if(mysqli_query($conn,$Actualizar)){
-            header("Location:Modificar_Conf.php?Nombre=$Nombre");
+            header("Location:Modificar_Conf.php?Nombre_Especialista=$Nombre_Especialista");
          } else{
             echo "Error: " . $sql . "<br>" . mysqli_error($conn);
         }
@@ -49,8 +64,8 @@
     }
 
 	if(isset($_REQUEST['id'])){ 
-        $id_Articulo=$_REQUEST['id'];     
-        $sql= " SELECT * FROM Articulos WHERE id_Articulo= '$id_Articulo'; ";
+        $DNI_Especialista=$_REQUEST['id'];     
+        $sql= " SELECT * FROM ESPECIALISTAS WHERE DNI_Especialista= '$DNI_Especialista'; ";
 		$result= mysqli_query($conn,$sql);
 
             if(mysqli_num_rows($result)>0)
@@ -64,21 +79,42 @@
                     </div>
                     <form id="ModificaArticulo" action="" method="post">
                         <!--- ID readonly, ya que es el unico que no se podrá modificar --->
-                        <label for="id_Articulo">Id del Articulo:</label>
-                            <input type="text" id="id_Articulo" readonly name="id_Articulo" class="caja" autofocus value = '<?php echo $row['id_Articulo'] ?>' placeholder="id_Articulo">
-                        <label for="Nombre">Nombre:</label>
-                            <input type="text" id="Nombre" name="Nombre" class="caja" autofocus required value = '<?php echo $row['Nombre'] ?>' placeholder="Nombre">
-                        <label for="Descripcion">Descripcion:</label>
-                            <input type="text" id="Descripcion" name="Descripcion" class="caja" required value = '<?php echo $row['Descripcion'] ?>' placeholder="Descripcion">
-                        <label for="Foto">Foto:</label>
-                            <input type="image" name="Foto" id="Foto" class="caja" value = '<?php echo $row['Foto'] ?>' placeholder="Foto">
-                        <label for="Precio">Precio: </label>
-                            <input type="number" name="Precio"  id="Precio" class="caja" required value = '<?php echo $row['Precio'] ?>' placeholder="Precio">
+                        <label for="DNI_Especialista">Id del Articulo:</label>
+                            <input type="text" id="DNI_Especialista" readonly name="DNI_Especialista" class="caja" autofocus value = '<?php echo $row['DNI_Especialista'] ?>' placeholder="DNI_Especialista">
+                        <label for="Nombre_Especialista">Nombre:</label>
+                            <input type="text" id="Nombre_Especialista" name="Nombre_Especialista" class="caja" autofocus required value = '<?php echo $row['Nombre_Especialista'] ?>' placeholder="Nombre_Especialista">
+                        <label for="Apellido_Especialista">Apellido:</label>
+                            <input type="text" id="Apellido_Especialista" name="Apellido_Especialista" class="caja" required value = '<?php echo $row['Apellido_Especialista'] ?>' placeholder="Apellido_Especialista">
+                        <label for="FechaNacimiento_Especialista">Fecha de nacimiento:</label>
+                            <input type="image" name="FechaNacimiento_Especialista" id="FechaNacimiento_Especialista" class="caja" value = '<?php echo $row['FechaNacimiento_Especialista'] ?>' placeholder="FechaNacimiento_Especialista">
+                        <label for="NumTelefono_Especialista">Numero de telefono: </label>
+                            <input type="number" name="NumTelefono_Especialista"  id="NumTelefono_Especialista" class="caja" required value = '<?php echo $row['NumTelefono_Especialista'] ?>' placeholder="NumTelefono_Especialista">
+
+                        <label for="Correo_Especialista">Correo electronico: </label>
+                            <input type="number" name="Correo_Especialista"  id="Correo_Especialista" class="caja" required value = '<?php echo $row['Correo_Especialista'] ?>' placeholder="Correo_Especialista">
+
+                        <label for="TipoVia_Especialista">Tipo de via: </label>
+                            <input type="number" name="TipoVia_Especialista"  id="TipoVia_Especialista" class="caja" required value = '<?php echo $row['TipoVia_Especialista'] ?>' placeholder="TipoVia_Especialista">
+
+                        <label for="NombreVia_Especialista">Nombre de la via: </label>
+                            <input type="number" name="NombreVia_Especialista"  id="NombreVia_Especialista" class="caja" required value = '<?php echo $row['NombreVia_Especialista'] ?>' placeholder="NombreVia_Especialista">
+
+                        <label for="NumeroVia_Especialista">Numero de la via: </label>
+                            <input type="NumeroVia_Especialista" name="NumeroVia_Especialista"  id="NumeroVia_Especialista" class="caja" required value = '<?php echo $row['NumeroVia_Especialista'] ?>' placeholder="NumeroVia_Especialista">
+
+                        <label for="CuentaBancaria_Especialista">Cuenta bancaria: </label>
+                            <input type="number" name="CuentaBancaria_Especialista"  id="CuentaBancaria_Especialista" class="caja" required value = '<?php echo $row['CuentaBancaria_Especialista'] ?>' placeholder="CuentaBancaria_Especialista">
+
+                        <label for="Cuota_Especialista">Cuota del especialista: </label>
+                            <input type="number" name="Cuota_Especialista"  id="Cuota_Especialista" class="caja" required value = '<?php echo $row['Cuota_Especialista'] ?>' placeholder="Cuota_Especialista">
+
+                        <label for="Contrasena_Especialista">Contraseña: </label>
+                            <input type="number" name="Contrasena_Especialista"  id="Contrasena_Especialista" class="caja" required value = '<?php echo $row['Contrasena_Especialista'] ?>' placeholder="Contrasena_Especialista">
                         
                         <button type="submit" title="Modificar" name="Modificar">Modificar</button>
                     </form>
                     <div class="pie-form">
-                        <a href="catalogo.php">Volver</a>
+                        <a href="inicio.php">Volver</a>
                     </div>
                 </div>
             </div>    
@@ -91,12 +127,7 @@
       }
 ?>
 
-
-
-
-ConfirmacionModificar.php
-<button><>
-
+<a href="ConfirmacionModificar.php">Confirmar registro</a>
 
 <!-- PIE DE PAGINA -->
 <footer>
