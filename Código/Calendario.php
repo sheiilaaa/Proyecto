8765508f22beb91a258aafec5f07ac1ac52263e2
Len-26 <elenadalmau6@gmail.com>
2024-10-31 19:34:31 +0100

    main

--- Código/Calendario.php
+++ Código/Calendario.php
@@ -1,8 +1,35 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
+   <!-- <title>Calendar</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+        }
+        .centrar_contenido {
+            display: flex;
+            width: 70%;
+        }
+        .calendario {
+            display: flex;
+            flex-wrap: wrap; 
+            justify-content: space-between; 
+        }
+        .dias_calendario {
+            margin: 5px;
+            border: solid 1px #ccc;
+            border-radius: 5px;
+            width: calc(14.2857% - 10px);
+            text-align: center; 
+            padding: 20px; 
+        }
+    </style>
+=======
     <title>ComoTrabajamos</title>
     <link rel="stylesheet" href="css/estilo.css">
     <style>
@@ -28,10 +55,28 @@
             padding: 20px; 
         }
     </style>
+>>>>>>> 4887e5de1922603efd856299063bc1bc4fa30e0a
 </head>
 
-<body>
+    -->
 
+
+
+<body>
+<<<<<<< HEAD
+    <main class="centrar_contenido">
+        <section class="calendario">
+            <?php
+                $diasEnElMes = 31;
+                $primerDiaDeLaSemana = 0; 
+=======
+>>>>>>> 4887e5de1922603efd856299063bc1bc4fa30e0a
+
+<<<<<<< HEAD
+
+                for ($i = 0; $i < $primerDiaDeLaSemana; $i++) {
+                    echo "<div class='dias_calendario'></div>";
+=======
     <!--CABECERA-->
     <header id="header">
         <nav class="menu">
@@ -58,12 +103,22 @@
 
                 for ($i = 0; $i < $primerDiaDeLaSemana; $i++) {
                     echo "<div class='dias_calendario'></div>";
+>>>>>>> 4887e5de1922603efd856299063bc1bc4fa30e0a
                 }
 
                 for ($contador = 1; $contador <= $diasEnElMes; $contador++) {
                     echo "<div class='dias_calendario'>$contador</div>";
                 }
 
+<<<<<<< HEAD
+                $totalDias = $primerDiaDeLaSemana + $diasEnElMes;
+                for ($i = $totalDias; $i < 35; $i++) {
+                    echo "<div class='dias_calendario'></div>";
+                }
+            ?>
+        </section>
+    </main>
+=======
                 $totalDias = $primerDiaDeLaSemana + $diasEnElMes;
                 for ($i = $totalDias; $i < 35; $i++) {
                     echo "<div class='dias_calendario'></div>";
@@ -77,5 +132,6 @@
         Todos los derechos reservados | Coaching SL Copyright © 2024
     </footer>
     
+>>>>>>> 4887e5de1922603efd856299063bc1bc4fa30e0a
 </body>
 </html>
