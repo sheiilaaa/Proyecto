71a5f24a082f13cc6f6f9cc31974de2f9e97b775
Len-26 <elenadalmau6@gmail.com>
2024-11-19 20:25:25 +0100

    main

--- Código/ListadoEspecialistas.php
+++ Código/ListadoEspecialistas.php
@@ -17,16 +17,15 @@
         <!-- Link hacia el archivo de estilos de bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-        <script src="script.js"></script>
+        <script src="script_listado.js"></script>
     </head>
 
-    <body class="fondo">
-<!-- CONEXION -->
+  <body class="fondo"> <!-- Falta comprobar-->
+<!--CONEXIÓN-->
 <?php
     session_start();
-    include ("./GestionBD/conexion.php");
+    include("./GestionBD/conexion.php");
 ?>
-
 
 <!--CABECERA-->
   <section class="photo" id="inicio">
@@ -48,71 +47,72 @@
         <div class="overlay"></div>
     </section>
 
+
+<!-- Listado de especialistas -->
+
     <!-- Link hacia las librerias jsp de bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-      <div class="titulo2"> Listado de especialistas </div>
-      <div id="contenedor2">
+<div class="titulos"> Listado de especialistas </div>
+<div id="fondo_listado">
+    <?php
 
-<?php
-  $sql="SELECT * FROM articulos";  
-  $result = mysqli_query($conn,$sql);
-  
-  if (mysqli_num_rows($result)>0){ //Si encuentra resultados
+      $sql="SELECT * FROM especialistas";  
+      $result = mysqli_query($conn,$sql);
+      
+      if (mysqli_num_rows($result)>0){ //Si encuentra resultados
+      while($row = mysqli_fetch_assoc($result)){
+    ?>
+    <div>
+      <!-- <div class="card" style="width: 18rem; margin: 10px;">
+        <img src="  <?php   /* echo $row['Foto']  */   ?>" class="card-img-top" alt="img3">
+      -->
 
-  ?>
-    <a href="AltaArticulo.php?"> Alta Artículo</a> <!--- enlace de la izquierda --->
+        <div class="card-body">
+          <h5 class="card-title">Nombre: <?php echo $row['Nombre_Especialista']?> <?php echo $row['Apellido_Especialista']?></h5>
+          <p class="card-text">Especialidades: <?php echo $row['Especialidad_Especialista']?></p> <!-- Como se hace-->
+          <p class="card-text">Cuota: <?php echo $row['Cuota_Especialista']?><label name="Precio4"></label></p>
+          <p class="card-text">Disponibilidad: <?php echo $row['Disponibilidad_Especialista']?></p> <!-- Como se hace-->
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+              <input type="button" id="cantidad4" name="Añadir4" class="boton" value="Pedir Cita"> 
+            <?php
+              if ($_SESSION['Nombre_Cliente']== "Admin"){  //Debe ser NOMBRECLIENTE O QUE?
+                //Si el usuario es Admin
+            ?>
+            <!--- enclaces para modificar o eliminar cada articulo --->
+              <a href="ModificarEspecialista.php?id=<?php $row['ID_Especialista']?>"> Modificar </a> 
+                <br>
+              <a href="EliminarEspecialista.php?id=<?php $row['ID_Especialista']?>"> Eliminar </a>
+                <br>
+              <?php
+                }
+              ?>
+            </li>
+        </ul>
+      </div>
+  </div>
+
+
+
+
     <br>
-  <?php
-    while($row = mysqli_fetch_assoc($result)){
-
-	?>
-           <div>
-                <div class="card" style="width: 18rem; margin: 10px;">
-                    <img src="<?php echo $row['Foto']?>" class="card-img-top" alt="img3">
-                    <div class="card-body">
-                      <h5 class="card-title">Nombre: <?php echo $row['Nombre']?></h5>
-                      <p class="card-text">Descripcion: <?php echo $row['Descripcion']?></p>
-                      <p class="card-text">Precio: <?php echo $row['Precio']?><label name="Precio4"></label></p>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item">
-                        <label for="cantidad4">Cantidad: </label>
-                        <input  type="number" id="cantidad4" style="width: 80px;">
-                        <input  type="button" id="cantidad4" name="Añadir4" class="boton" value="Añadir" onclick="ProdAdd('prod4','154')">
-                      
-                      <?php
-                        if ($_SESSION['Usuario']== "Admin"){ //Si el usuario es Admin
-                      ?>
-                                <!--- enclaces para modificar o eliminar cada articulo --->
-                          <a href="ModificarEspecialista.php?id=<?php $row['id_Articulo']?>"> Modificar </a> 
-                          <br>
-                          <a href="EliminarEspecialista.php?id=<?php $row['id_Especialista']?>"> Eliminar </a>
-                          <br>
-                      <?php
-						            }
-                      ?>
-                      </li>
-                    </ul>
-                </div>
-            </div>
-            <br>
-<?php
+  <?php   /*
     }
-      if ($_SESSION['Usuario']== "Admin"){ //Si el usuario es Admin, permite añadir articulos
-      ?>
-      <br>
-      <a href="AltaArticulo.php?"> Alta Artículo</a>  <!--- enlace de la derechca --->
-<?php
+    if ($_SESSION['Usuario']== "Admin"){ //Si el usuario es Admin, permite añadir articulos
+  ?>
+  <br>
+    <a href="AltaArticulo.php?"> Alta Artículo</a>  <!--- enlace de la derechca --->
+  <?php  */
+  
+    }
   }
-}
-?>      
-      </div> <!--- cierra div contenedor2--->
+
+
+  ?>      
+</div> <!--- cierra div contenedor2--->
           
-        <span id="contenedor2">
-            <h5><a href="login.php"> Inicio </a></h5>
-        </span>  
-        
 
 <!-- PIE DE PAGINA -->
 <footer>
