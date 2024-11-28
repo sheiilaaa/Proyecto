4aff080178f32eec6265790926e163776500a3e4
Len-26 <elenadalmau6@gmail.com>
2024-11-28 19:02:36 +0100

    main

--- Código/ListadoEspecialistas.php
+++ Código/ListadoEspecialistas.php
@@ -1,141 +1,90 @@
+<?php
+session_start();
+include("./GestionBD/1-conexion.php");
+?>
+
 <!DOCTYPE html>
 <html lang="es"> 
-    <head>
-        <meta charset="utf-8">
+<head>
+    <meta charset="utf-8">
+    <title>Listado Especialista</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <!--<script src="script_listado.js" defer></script>-->
+</head>
 
-        <title> Listado Especialista </title>    
-  
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         
-        <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet"> 
-        <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
-        
-       
-        <!-- Link hacia el archivo de estilos css -->
-        <link rel="stylesheet" href="css/estilo.css">
-        <!-- Link para que funcionen los FA FA -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-
-        <script src="script_listado.js"></script>
-    </head>
-
-  <body class="fondo"> <!-- Falta comprobar-->
-<!--CONEXIÓN-->
-        <?php
-            session_start();
-            include("./GestionBD/1-conexion.php");
-        ?>
-
+<body class="fondo">
 <!--CABECERA-->
-        <section class="photo" id="inicio">
-            <div class="nav" id="sticker">
-                <label for="toggle">&#9776</label>
-                <input type="checkbox" id="toggle" />
-                <div class="menu">
-                    <img src="IMG/logo.png" alt="" class="logo">
-                    <a href=""><i class="fa fa-home"> Inicio</i></a>
-                    <a href=""><i class="fa fa-info"> Como trabajar</i></a>
-                    <a href=""><i class="fa fa-briefcase"> Puesta en contacto</i></a>
-                    <a href=""><i class="fa fa-address-book"> Listado especialistas</i></a>
-                    <a href=""><i class="fa fa-calendar">Calendario</i></a>
-                </div>
-            </div>
-            <div class="photo-text">
-                <h4 data-ix="skype">Coaching sl</h4>
-            </div>
-            <div class="overlay"></div>
-        </section>
-
+<section class="photo" id="inicio">
+    <div class="nav" id="sticker">
+        <label for="toggle">&#9776</label>
+        <input type="checkbox" id="toggle" />
+        <div class="menu">
+            <img src="IMG/logo.png" alt="" class="logo">
+            <a href=""><i class="fa fa-home"> Inicio</i></a>
+            <a href=""><i class="fa fa-info"> Cómo trabajar</i></a>
+            <a href=""><i class="fa fa-briefcase"> Puesta en contacto</i></a>
+            <a href=""><i class="fa fa-address-book"> Listado especialistas</i></a>
+            <a href=""><i class="fa fa-calendar"> Calendario</i></a>
+        </div>
+    </div>
+    <div class="photo-text">
+        <h4 data-ix="skype">Coaching SL</h4>
+    </div>
+    <div class="overlay"></div>
+</section>
 
 <!-- Listado de especialistas -->
-        <!-- Link hacia las librerias jsp de bootstrap -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<div class="titulos">Listado de especialistas</div>
+<div id="fondo_listado">
+    <?php
+    $sql = "SELECT E.Nombre_Especialista, E.Apellido_Especialista, ES.Especialidad_Especialista, E.Cuota_Especialista
+            FROM ESPECIALISTAS E
+            JOIN ESPECIALISTA_ESPECIALIDAD EE ON E.ID_Especialista = EE.ID_Especialista_EspeEspe
+            JOIN ESPECIALIDAD ES ON EE.ID_Especialidad_EspeEspe = ES.ID_Especialidad";
 
-        <div class="titulos"> Listado de especialistas </div>
-        <div id="fondo_listado">
-            <?php
-            /* JOIN */
-            $sql="SELECT E.Cuota_Especialista, E.Nombre_Especialista, E.Apellido_Especialista, ES.Especialidad_Especialista
-                    FROM ESPECIALISTAS E
-                    JOIN ESPECIALISTA_ESPECIALIDAD EE ON E.ID_Especialista = EE.ID_Especialista_EspeEspe
-                    JOIN ESPECIALIDAD ES ON ES.ID_Especialista = E.ID_Especialidad_EspeEspe";
+    $result = mysqli_query($conn, $sql);
 
-            $sql1="SELECT DE.Fecha_Disponibilidad, DE.Hora_Disponibilidad, DE.Disponibilidad_Especialista
-                    FROM DISPONIBILIDAD_ESPECIALISTA DE
-                    JOIN ESPECIALISTAS E ON E.ID_Especialista = DE.ID_Especialista_DispoEspe";
+    if (mysqli_num_rows($result) > 0) { // Si encuentra resultados
+        $Final = 0;
+        $row = mysqli_fetch_assoc($result);
 
-            $result = mysqli_query($conn,$sql);
-            $result1 = mysqli_query($conn,$sql1);
+        while ($row) { 
+            $Esp_Anterior = $row['Nombre_Especialista'];
+            echo '<div class="especialista-contenedor">';
+            echo '<h5>Especialista: '.$row['Nombre_Especialista'].' '.$row['Apellido_Especialista'].'</h5>';
+            echo '<p>Cuota: '.$row['Cuota_Especialista'].'€</p>';
+            echo '<ul>';
 
-            if (mysqli_num_rows($result)>0){ //Si encuentra resultados
-            while($row = mysqli_fetch_assoc($result)){
-                $Final = 0
-            $row = mysqli_fetch_assoc($result)
-                while($Final<= $result) /*1. controlo que el especialista anetrior sea igual que el actual, por lo que se que esa especilidad es de ese especialista*/
-                $Esp_Anterior = $row[ESPECIALISTAS]
-                <div>
-                ESPECIALISTAS.value = $row[ESPECIALISTAS]
-                    $i=1
-                while $Esp_Anterior == $row[ESPECIALISTAS]
-                {
-                    ESPECIALIDADi.value = $row[ESPECIALIDAD]
-                    $Final++
-                    $i++
-                    $row = mysqli_fetch_assoc($result) /*con esto salto de línea*/
-                }
-                </div>
-                                
-
-                                        /*2. que muestre todas las especialidades*/
-            ?>
-        
-            <div>
-                <div class="card" style="width: 18rem; margin: 10px;"><!--
-                <img src="  <?php   /* echo $row['Foto']  */   ?>" class="card-img-top" alt="img3">-->
-                <div class="card-body">
-                    <h5 class="card-title">Nombre: <?php echo $row['Nombre_Especialista']?> <?php echo $row['Apellido_Especialista']?></h5>
-                    <p class="card-text">Especialidades: <?php echo $row['Especialidad_Especialista']?></p> <!-- Como se hace-->
-                    <p class="card-text">Cuota: <?php echo $row['Cuota_Especialista']?><label name="Precio4"></label></p>
-                    <p class="card-text">Disponibilidad: <?php echo $row['Disponibilidad_Especialista']?></p> <!-- Como se hace-->
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">
-                    <input type="button" id="cantidad4" name="Añadir4" class="boton" value="Pedir Cita"> 
-                    <?php
-                      if ($_SESSION['usuario']== "Admin"){  //Debe ser NOMBRECLIENTE O QUE?
-                      //Si el usuario es Admin
-                    ?>
-                    <!--- enclaces para modificar o eliminar cada articulo --->
-                    <a href="ModificarEspecialista.php?id=<?php $row['ID_Especialista']?>"> Modificar </a> 
-                    <br>
-                    <a href="EliminarEspecialista.php?id=<?php $row['ID_Especialista']?>"> Eliminar </a>
-                    <br>
-                    <?php
-                    }
-                    ?>
-                  </li>
-                </ul>
-                </div>
-            </div>
-
-            <br>
-            <?php /*
-                }
-                    if ($_SESSION['Usuario']== "Admin"){ //Si el usuario es Admin, permite añadir articulos
-                ?>
-                <br>
-                    <a href="AltaArticulo.php?"> Alta Artículo</a>  <!--- enlace de la derechca --->
-                <?php  */ 
-                
-                }
+            // Agrupar especialidades por especialista
+            $i = 1;
+            while ($row && $Esp_Anterior == $row['Nombre_Especialista']) {
+                echo '<li>Especialidad '.$i.': '.$row['Especialidad_Especialista'].'</li>';
+                $Final++;
+                $i++;
+                $row = mysqli_fetch_assoc($result); // Avanzar a la siguiente fila
             }
-            ?> 
-             
-        </div> <!--- cierra div--->       
+            echo '</ul>';
+            echo '<input type="button" id="cantidad4" name="Añadir4" class="boton" value="Pedir Cita">';
+            /*if ($_SESSION['usuario'] == "Admin") { // Si es Admin, mostrar opciones adicionales
+                echo '<br><a href="ModificarEspecialista.php?id='.$Esp_Anterior.'">Modificar</a>';
+                echo '<br><a href="EliminarEspecialista.php?id='.$Esp_Anterior.'">Eliminar</a>';
+            }
+            echo '</div>';
+            echo '<br>';*/
+        }
+    } else {
+        echo '<p>No se encontraron especialistas.</p>';
+    }
+    ?>
+</div>
 
 <!-- PIE DE PAGINA -->
-        <footer>
-        Todos los derechos reservados | Coaching SL Copyright © 2024
-        </footer>
-    </body>
+<footer>
+    Todos los derechos reservados | Coaching SL Copyright © 2024
+</footer>
+</body>
 </html>
