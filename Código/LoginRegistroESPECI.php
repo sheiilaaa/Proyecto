6dbf1757e2119e4497b15e99c3600ff29fc0d1d8
Len-26 <elenadalmau6@gmail.com>
2024-11-28 15:40:35 +0100

    main

--- Código/LoginRegistroESPECI.php
+++ Código/LoginRegistroESPECI.php
@@ -196,7 +196,8 @@
                             <label for="Contrasena_Especialista">Contraseña:</label>
                             <input type="password" name="Contrasena_Especialista" id="Contrasena_Especialista" class="caja"required placeholder="Escribe tu contraseña">
 
-                            <a href="ModificarEspecialista.php?id=<?php $row['ID_Especialista']?>"> Modificar </a> 
+                            <a href="DisponibilidadEspecialista.php?id=<?php $row['ID_Especialista']?>"> Registrar Especialidad</a> 
+                            
                             <button type="submit" title="AltaEspecialista" name="AltaEspecialista">Alta Especialista</button>
                               
                         </form>
