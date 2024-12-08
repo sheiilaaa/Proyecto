7e0735e44bf1f8c91dbe9855609d79d4a805c170
ElenaaDalmau <elenadalmau6@gmail.com>
2024-12-08 20:08:21 +0100

    main

--- Código/Inicio.php
+++ Código/Inicio.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Nunito&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Overpass&display=swap" rel="stylesheet">
+    
+    
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -34,11 +36,103 @@ if (!$conn) {
             <li><a href="Inicio.php"><i class="fa fa-home"></i> Inicio</a></li>
             <li><a href="ComoTrabajamos.php"><i class="fa fa-briefcase"></i> ¿Quiénes somos?</a></li>
             <li><a href="Contacto.php"><i class="fa fa-phone-square"></i> Puesta en contacto</a></li>
-            <li><a href="ListadoEspecialistas.php"><i class="fa fa-address-book"></i> Especialistas</a></li>
+            <li><a href="ListadoEspe.php"><i class="fa fa-address-book"></i> Especialistas</a></li>
             <li><a href="Calendario.html"><i class="fa fa-calendar"></i> Calendario</a></li>
         </ul>
     </nav>
 </div>
+
+<!-- Fondo Inicio -->
+<section class="inicioportada">
+        <div class="cajainicio">
+            <div class="textosportada">
+                <div class="coachingsl">COACHING S.L.</div>
+                <div class="textobienvenida">¡Bienvenido a la página que te va a cambiar la forma de ver tu vida!</div>
+            </div>
+        </div>
+</section>
+
+<!-- INFO NUESTRA -->
+<section id="uses">
+    <div class="container">
+      <div class="uses-copy">
+        <hr class="highlight"/>
+        <h2>¿Qué vas a encontrar aquí dentro?</h2>
+        <p>¿porqué esta página web y no otra página? ¿Porque nuestros servicios y no otros? A continuación, te explicaremos el porqué
+          deberías de escogernos a nosotros, y no a cualquier otra empresa:
+        </p>
+      </div>
+
+      <ul class="feature-cards">
+        <li>
+          <i class="fa-solid fa-user-tie lock-icon"></i>
+          </svg>
+          <h4 class="feature-title">
+            1. Ayuda profesional elegida por ti</h4>
+          <div class="feature-copy">
+            <p>
+              Aquí dentro, podrás escoger entre diversos profesionales aquel que creas que es el indicado para ti.</p>
+          </div>
+        </li>
+        
+        <li>
+        <i class="fa-solid fa-book-open lock-icon"></i>
+          <h4 class="feature-title">
+            2. La especialidad que más prefieras</h4>
+          <div class="feature-copy">
+            <p>
+              Podrás investigar que servicios ofrecemos y, de entre todas las opciones, escoger aquella que creas que
+              creas que se adapta mejor a tus necesidades.</p>
+          </div>
+        </li>
+
+        <li>
+          <i class="fa-regular fa-lightbulb lock-icon"></i>
+          <h4 class="feature-title">
+            3. ¿No sabes que hacer?</h4>
+          <div class="feature-copy">
+            <p>
+              Ofrecemos la posibilidad de contactar con nosotros para pedir consejos. ¡Nosotros siempre estaremos dispuestos a ayudarte!</p>
+          </div>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-calendar-days lock-icon"></i>
+          <h4 class="feature-title">
+            4. Modificar fechas</h4>
+          <div class="feature-copy">
+            <p>
+              Si has reservado una cita y, cuando se acerca la fecha, te das cuenta que finalmente no podrás asistir, ¡no te preocupes!.
+              Ofrecemos la opción de anular citas programadas sin consecuencias.</p>
+            </div>
+        </li>
+
+        <li>
+          <i class="fa-solid fa-lock lock-icon"></i>
+
+          <h4 class="feature-title">
+            5. Privacidad</h4>
+          <div class="feature-copy">
+            <p>
+              Ofrecemos privacidad con todo aquello que cuentes: nadie se va a enterar de las cosas que
+              comentas o explicas con nuestros especialistas.</p>
+          </div>
+        </li>
+
+        <li>
+          <i class="fa-regular fa-credit-card lock-icon"></i>
+          <h4 class="feature-title">
+            6. Método de pago</h4>
+          <div class="feature-copy">
+            <p>
+              Podrás escoger que método de pago deseas utilizar, pudiendo pagar en efectivo o desde la web.
+              Ofrecemos la opción de poder pagar una cantidad antes de realizar la sesión.</p>
+          </div>
+        </li>
+    </div>
+    </div>
+  </section>
+
 
 <div class="container" id="container">
 
@@ -159,5 +253,17 @@ if (!$conn) {
         <a href="inicioESPE.php">Iniciar Sesión Especialista</a>
     </div>
 </div>
+
+
+<!-- PIE DE PAGINA -->
+<footer>
+    Todos los derechos reservados | Coaching SL Copyright © 2024
+</footer>
+
+    <!-- Link a JavaScript -->
+    <script src="JS/traducciones.js"></script>
+
+
+
 </body>
 </html>
